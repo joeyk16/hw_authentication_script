e8512649e06d538b23c5eb608d2310eac4096dc6
Daniel Zambrano <cacique.coder@gmail.com>
2017-07-14 13:28:50 +1000

    first changes

---
+++ hw_authentication.js
@@ -0,0 +1,26 @@
+var scriptTags = document.querySelectorAll('script');
+var redirectUrl, domain, clientId;
+for(var i = scriptTags.lenght - 1, i >= 0 ; i--){
+  if (/hw\_authentication/.test(scriptTags[i].src)){
+    redirectUrl = scriptTags[i].dataset.redirectUrl;
+    clientId = scriptTags[i].dataset.clientId;
+    domain = scriptTags[i].dataset.domain;
+   break;
+  }
+}
+
+if (redirectUrl === undefined){
+  console.error('Please include on this script the redirectUrl data attribute');
+  alert('Hw authentication is not correctly configure');
+}
+
+var lock = new Auth0Lock(clientId, domain,{
+    auth: {
+      redirectUrl: redirectUrl,
+      responseType: 'code',
+      params: {
+        scope: 'openid email' // Learn about scopes: https://auth0.com/docs/scopes
+      }
+    }
+  });
+lock.show();
